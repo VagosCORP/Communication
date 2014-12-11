34614afc0a4646a86a2d7dcdc69024d6bd43529c
Sebasu <Sebasu_Knutti@hotmail.com>
2014-12-11 14:42:42 -0400

    First Upload (Android Studio Project)

---
+++ src/main/java/vclibs/communication/android/TimeOut.java
@@ -0,0 +1,67 @@
+package vclibs.communication.android;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import vclibs.communication.Eventos.OnTimeOutListener;
+
+//Clase Temporizador, espera un tiempo en un hilo secundario
+public class TimeOut extends AsyncTask<Long, Void, Integer> {
+	
+	//Variables para seleccionar qué imprimir en la Consola
+	public boolean idebug = true;
+	public boolean edebug = true;
+	
+	OnTimeOutListener onTOListener;
+	
+	public void setTimeOutListener(OnTimeOutListener tOListener) {
+		onTOListener = tOListener;
+	}
+	
+	//Acciones anteriores al inicio del hilo de ejecusión secundario
+	@Override
+	protected void onPreExecute() {
+		if(idebug)
+			Log.d("TimeOut", "onPreExecute");
+		if(onTOListener != null)
+			onTOListener.onTimeOutEnabled();
+		super.onPreExecute();
+	}
+
+	//Función del hilo de ejecución secundario
+	@Override
+	protected Integer doInBackground(Long... params) {
+		if(idebug)
+			Log.d("TimeOut", "doInBackground");
+		long ms = params[0]/10;
+		try {
+			for(int i=0; !isCancelled() && i < ms; i++) {
+				Thread.sleep(10);
+			}
+		} catch (InterruptedException e) {
+			if(edebug)
+				e.printStackTrace();
+		}
+		return 1;
+	}
+
+	//Acciones ante cancelaci�n de Actividad del hilo
+	@Override
+	protected void onCancelled() {
+		if(idebug)
+			Log.d("TimeOut", "onCancelled");
+		if(onTOListener != null)
+			onTOListener.onTimeOutCancelled();
+		super.onCancelled();
+	}
+
+	//Acciones ante la finalizaci�n de acciones del hilo
+	@Override
+	protected void onPostExecute(Integer result) {
+		if(idebug)
+			Log.d("TimeOut", "onPostExecute");
+		if(onTOListener != null)
+			onTOListener.onTimeOut();
+		super.onPostExecute(result);
+	}
+}
